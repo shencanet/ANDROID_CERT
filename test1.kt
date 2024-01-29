67240a47f2fdd8d919d39a21a872f7c56960cb72
Santi Dionis <96749556+shencanet@users.noreply.github.com>
2024-01-29 22:01:04 +0100

    Update test1.kt

--- test1.kt
+++ test1.kt
@@ -1,1 +1,10 @@
+fun main() {
+    val estudiante1 = Student("Juanma")
+    println(estudiante1.isLoggedIn)
+    estudiante1.login()
+    estudiante1.isLoggedIn = true
+    println(estudiante1.isLoggedIn)
+    estudiante1.logout()
+    estudiante1.isLoggedIn = false
 
+}
